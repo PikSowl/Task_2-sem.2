c93ead7e9450d9b182a6202e2d5a53f79e422be9
PikSowl <kmazalov11@gmail.com>
2023-02-04 12:06:18 +0200

    Rework using memcpy_s

--- main.cpp
+++ main.cpp
@@ -1,27 +1,19 @@
 #include <iostream>
 #include <experimental/random>
+#include <memory.h>
 typedef int some_type; // int can be switch on something that isn't bool
 
 using std::cout;
 using std::endl;
 
 int main() {
-    const int N = 1000; //scalable to limits of int what is reasonably big
+    const int N = 100; //scalable to limits of int what is reasonably big
     some_type in_data[N];
     for (int i = 0; i < N;++i) in_data[i] = std::experimental::randint(32, 126); //numbers are good to convert into char
 
     some_type out_data[N] {0};
 
-    int small_n = N/(8/sizeof(some_type))+1;
-
-
-    double * in_point = (double*)in_data;
-    double * out_point = (double*)out_data;
-
-    for(int i = 0;i < small_n;i++){
-        *out_point++ = *in_point++;
-    }
-    *(double*)in_data = *(double*)out_data;//program corrupts first eight bytes of original data, patched it in straightforward manner
+    errno_t err = memcpy_s(out_data, N * sizeof(some_type), in_data, N * sizeof(some_type));
 
     for (some_type t : in_data)
     {
